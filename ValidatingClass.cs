242aa65a1c9d81ca6b63da75928a2cc405334ee4
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-12-07 12:54:19 +0300

    Add files via upload

---
+++ ValidatingClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ITMO.Course124.Exercise3._4
+{
+    public class ValidatingClass : System.Windows.Forms.TextBox
+    {
+             
+        protected override void OnValidating(System.ComponentModel.CancelEventArgs e)  
+        {
+            if (Text == "")
+            {
+                e.Cancel = true;
+                System.Windows.Forms.MessageBox.Show("Вы не заполнили поле");  
+            }
+            //else if (nText = double.Parse(nText))
+            //{ 
+            //    //try
+            //    //{
+            //    //    double.Parse(nText);
+            //    //    e.Cancel = false;
+            //    //}
+            //    //catch
+            //    //{
+            //    //    e.Cancel = true;
+            //    //    System.Windows.Forms.DialogResult dialogResult = System.Windows.Forms.MessageBox.Show("Поле ТЕЛЕФОН не может содержать буквы");
+            //    //    //nText = "Поле PIN не может содержать буквы";
+            //    //}
+            //}
+        }     
+  
+    }
+}
