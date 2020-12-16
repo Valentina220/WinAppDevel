0e4fbb0e313ff8b42c016a87a371f3249033ce02
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-12-16 15:24:19 +0300

    Add files via upload

---
+++ Calculator/CalcEngin.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SimpleCalculator
+{
+    partial class CalcEngin
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // KeyExit
+            // 
+            this.KeyExit.Location = new System.Drawing.Point(295, 212);
+            // 
+            // KeyDate
+            // 
+            this.KeyDate.Location = new System.Drawing.Point(295, 164);
+            // 
+            // KeyClear
+            // 
+            this.KeyClear.Location = new System.Drawing.Point(295, 116);
+            // 
+            // KeyEqual
+            // 
+            this.KeyEqual.Location = new System.Drawing.Point(295, 260);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(203, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "x^y";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(203, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "√x";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(203, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "1/x";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(203, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 40);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "x^2";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(249, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 40);
+            this.button5.TabIndex = 27;
+            this.button5.Text = "x!";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(249, 166);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(40, 40);
+            this.button6.TabIndex = 28;
+            this.button6.Text = "∛x";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // CalcEngin
+            // 
+            this.ClientSize = new System.Drawing.Size(358, 328);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "CalcEngin";
+            this.Text = "Engineering Calculator";
+            this.Load += new System.EventHandler(this.CalcEngin_Load);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.button3, 0);
+            this.Controls.SetChildIndex(this.button4, 0);
+            this.Controls.SetChildIndex(this.button5, 0);
+            this.Controls.SetChildIndex(this.button6, 0);
+            this.Controls.SetChildIndex(this.VersionInfo, 0);
+            this.Controls.SetChildIndex(this.OutputDisplay, 0);
+            this.Controls.SetChildIndex(this.KeyExit, 0);
+            this.Controls.SetChildIndex(this.KeyClear, 0);
+            this.Controls.SetChildIndex(this.KeyDate, 0);
+            this.Controls.SetChildIndex(this.KeyEqual, 0);
+            this.Controls.SetChildIndex(this.KeyPoint, 0);
+            this.Controls.SetChildIndex(this.KeySign, 0);
+            this.Controls.SetChildIndex(this.KeyDivide, 0);
+            this.Controls.SetChildIndex(this.KeyMultiply, 0);
+            this.Controls.SetChildIndex(this.KeyMinus, 0);
+            this.Controls.SetChildIndex(this.KeyPlus, 0);
+            this.Controls.SetChildIndex(this.KeyZero, 0);
+            this.Controls.SetChildIndex(this.KeyNine, 0);
+            this.Controls.SetChildIndex(this.KeyEight, 0);
+            this.Controls.SetChildIndex(this.KeySeven, 0);
+            this.Controls.SetChildIndex(this.KeySix, 0);
+            this.Controls.SetChildIndex(this.KeyFive, 0);
+            this.Controls.SetChildIndex(this.KeyFour, 0);
+            this.Controls.SetChildIndex(this.KeyThree, 0);
+            this.Controls.SetChildIndex(this.KeyTwo, 0);
+            this.Controls.SetChildIndex(this.KeyOne, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
