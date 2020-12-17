9f136c892a9133ffa78902ad15212846d02a5558
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-12-17 15:00:33 +0300

    Add files via upload

---
+++ LabRab1/Exercise1.5.2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Course124.Exercise1._5._3
+{
+    public partial class GreenForm : Form
+    {
+        public GreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            //myPath.AddRectangle(0, 0, this.Width, this.Height);
+            myPath.AddLines(new[]
+           {
+                 new Point(0, this.Height / 2),
+                 new Point(this.Width / 2, 0),
+                 new Point(this.Width, this.Height / 2),
+                 new Point(this.Width / 2, this.Height)
+             });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ LabRab1/Exercise1.5.2/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Course124.Exercise1._5._3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GreenForm f = new GreenForm();
+            f.ShowDialog();        
+        }
+    }
+}
