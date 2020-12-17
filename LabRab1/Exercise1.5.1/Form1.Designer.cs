c93b5f27b01e265915171c454800d45c4d3639ae
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-12-17 14:59:28 +0300

    Add files via upload

---
+++ LabRab1/Exercise1.5.1/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ITMO.Course124.Exercise1._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Closebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Closebutton
+            // 
+            this.Closebutton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Closebutton.Location = new System.Drawing.Point(270, 219);
+            this.Closebutton.Name = "Closebutton";
+            this.Closebutton.Size = new System.Drawing.Size(228, 53);
+            this.Closebutton.TabIndex = 0;
+            this.Closebutton.Text = "Close";
+            this.Closebutton.UseVisualStyleBackColor = false;
+            this.Closebutton.Click += new System.EventHandler(this.Closebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(769, 491);
+            this.Controls.Add(this.Closebutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Exercise1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Closebutton;
+    }
+}
+
